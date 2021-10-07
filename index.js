97370c45768f96e766efc69c0958673ff0df3825
cssmith07060 <cssmith07060@gmail.com>
2021-10-06 20:50:28 -0400

    commit tree 8bec23cdf7ff9402c3868cbe2f471469445181c3

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+const leastFavoriteCustomer = 'sour fred'
+
+function upperCaseCustomerName() {
+  return customerName = customerName.toUpperCase()
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'sour fred'
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+}
+
+function overWritesBestCustomer() {
+  bestCustomer = 'not bob'
+}
